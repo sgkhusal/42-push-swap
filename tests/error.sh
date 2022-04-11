6d57383bf7095f0f4374edfbfaff4d6ff93a92c7
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-04-11 21:19:18 +0200

    Add some invalid args tests and 000000 test

--- tests/error.sh
+++ tests/error.sh
@@ -30,12 +30,32 @@ echo "./push_swap 10 b 2 3 5 1 0 9 8"
 ./push_swap 10 b 2 3 5 1 0 9 8
 
 printf "\n"
+echo "./push_swap 10 2 3 Hello 1 0 9 8"
+./push_swap 10 2 3 Hello 1 0 9 8
+
+printf "\n"
 echo "./push_swap 10 4 \* 3 5 1 0 9 8"
 ./push_swap 10 4 \* 3 5 1 0 9 8
 
 printf "\n"
 echo "./push_swap 10 4 * 3 5 1 0 9 8"
 ./push_swap 10 4 * 3 5 1 0 9 8
+
+printf "\n"
+echo "./push_swap 10 1 2 3 5 0 +++7 9 8"
+./push_swap 10 1 2 3 5 0 +++7 9 8
+
+printf "\n"
+echo "./push_swap 10 1 2 3 5 ---7 9 8"
+./push_swap 10 1 2 3 5 ---7 9 8
+
+printf "\n"
+echo "./push_swap 10 1 2 3 5 +-+7 9 8"
+./push_swap 10 1 2 3 5 +-+7 9 8
+
+printf "\n"
+echo "./push_swap 10 1 2 3 5 -100+83 9 8"
+./push_swap 10 1 2 3 5 -100+83 9 8
 
 printf "\n"
 printf "$PURPLE"
@@ -52,8 +72,8 @@ echo "Test 3: number bigger than an integer"
 printf "$GREY"
 
 printf "\n"
-echo "./push_swap 10 1 2 3 5 0 2147483648 9 8"
-./push_swap 10 1 2 3 5 0 2147483648 9 8
+echo "./push_swap 10 1 2 3 5 0 9 2147483648 8"
+./push_swap 10 1 2 3 5 0 9 2147483648 8
 
 printf "\n"
 printf "$PURPLE"
@@ -61,8 +81,17 @@ echo "Test 4: number lesser than an integer"
 printf "$GREY"
 
 printf "\n"
-echo "./push_swap 10 1 2 3 5 0 -2147483649 9 8"
-./push_swap 10 1 2 3 5 0 -2147483649 9 8
+echo "./push_swap 10 1 2 3 5 0 9 -2147483649 8"
+./push_swap 10 1 2 3 5 0 9 -2147483649 8
+
+printf "\n"
+printf "$PURPLE"
+echo "Test 5: no error"
+printf "$GREY"
+
+printf "\n"
+echo "./push_swap 10 1 2 3 5 000000 9 8"
+./push_swap 10 1 2 3 5 000000 9 8
 
 printf "\n"
 printf "$END"
