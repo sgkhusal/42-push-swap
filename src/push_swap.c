9cc71e70a5344267ddd39a83e673a724a1e88347
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-04-13 19:25:49 +0200

    Add double linked list

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,51 +6,54 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 21:46:07 by sguilher          #+#    #+#             */
-/*   Updated: 2022/04/11 21:36:39 by sguilher         ###   ########.fr       */
+/*   Updated: 2022/04/13 19:24:27 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ps_error(long int *nbs)
+void	ps_error(t_stack *stack)
 {
-	if (nbs != NULL)
-		free(nbs);
+	if (stack != NULL)
+		free(stack);
 	ft_putstr_fd("Error\n", 2);
 	exit(EXIT_FAILURE);
 }
 
-void	ps_check_order(int stack_size, long int *nbs)
+void	ps_check_order(t_stack *stack)
 {
-	int	i;
+	t_dlist	*aux1;
+	t_dlist	*aux2;
 
-	i = 0;
-	while (i < stack_size - 1)
+	aux1 = stack->init;
+	aux2 = aux1->next;
+	while (aux2 != NULL)
 	{
-		if (nbs[i] > nbs[i + 1])
+		if (aux1->nb > aux2->nb)
 			return ;
-		i++;
+		aux1 = aux2;
+		aux2 = aux2->next;
 	}
-	free(nbs);
+	free(stack); //
 	exit(EXIT_SUCCESS);
 }
 
-void	ps_check_repeated_nb(int stack_size, long int *nbs)
+void	ps_check_repeated_nb(t_stack *stack)
 {
-	int	i;
-	int	j;
+	t_dlist	*aux1;
+	t_dlist	*aux2;
 
-	i = 0;
-	while (i < stack_size)
+	aux1 = stack->init;
+	while (aux1 != NULL)
 	{
-		j = i + 1;
-		while (j < stack_size)
+		aux2 = aux1->next;
+		while (aux2 != NULL)
 		{
-			if (nbs[i] == nbs[j])
-				ps_error(nbs);
-			j++;
+			if (aux1->nb == aux2->nb)
+				ps_error(stack);
+			aux2 = aux2->next;
 		}
-		i++;
+		aux1 = aux1->next;
 	}
 }
 
@@ -77,38 +80,85 @@ int	ps_check_char(char *nb)
 	return (OK);
 }
 
-int	*ps_check_args(int stack_size, char *numbers[])
+void	ps_fill_stack_a(t_stack *stack, int stack_size, char *numbers[])
 {
 	int	i;
-	long int	*nbs;
+	long int	nb;
+	t_dlist	*aux;
+	t_dlist	*tmp;
 
-	nbs = (long int *) malloc(stack_size * sizeof(long int));
-	if (nbs == NULL)
-		ps_error(NULL);
-	i = 0;
+	i = 1;
+	aux = stack->init;
 	while (i < stack_size)
 	{
 		if (ps_check_char(numbers[i]) == E_NOT_INT)
-			ps_error(nbs);
-		nbs[i] = ft_atol(numbers[i]);
-		if (nbs[i] > 2147483647 || nbs[i] < -2147483648)
-			ps_error(nbs);
+			ps_error(stack);
+		nb = ft_atol(numbers[i]);
+		if (nb > 2147483647 || nb < -2147483648)
+			ps_error(stack);
+		tmp = ft_dlstnew((int) nb);
+		if (tmp == NULL)
+			ps_error(stack);
+		tmp->prev = aux;
+		aux->next = tmp;
+		aux = tmp;
 		i++;
 	}
+	stack->end = aux;
+	ps_check_repeated_nb(stack);
+	ps_check_order(stack);
+}
+
+t_stack	*ps_init_stack_a(int stack_size, char *numbers[])
+{
+	t_stack		*stack;
+	long int	nb;
+	
+	if (ps_check_char(numbers[0]) == E_NOT_INT)
+		ps_error(NULL);
 	if (stack_size == 1)
-	{
-		free(nbs);
 		exit(EXIT_SUCCESS);
+	else
+	{
+		stack = (t_stack *)malloc(sizeof(t_stack));
+		if (stack == NULL)
+			ps_error(NULL);
+		nb = ft_atol(numbers[0]);
+		if (nb > 2147483647 || nb < -2147483648)
+			ps_error(stack);
+		stack->init = ft_dlstnew((int) nb);
+		if (stack->init == NULL)
+			ps_error(stack);
+		stack->end = NULL;
+		ps_fill_stack_a(stack, stack_size, numbers);
 	}
-	ps_check_repeated_nb(stack_size, nbs);
-	ps_check_order(stack_size, nbs);
-	return ((int *)nbs);
+	return (stack);
 }
 
 int	main(int argc, char *argv[])
 {
+	t_push_swap	data;
+	t_dlist	*tmp;
+	
 	if (argc == 1)
 		return (0);
-	ps_check_args(argc - 1, &argv[1]);
+	data.stack_size = argc - 1;
+	data.stack_a = ps_init_stack_a(data.stack_size, &argv[1]);
+	tmp = data.stack_a->init;
+	ft_printf("From double linked list:\n");
+	while (tmp != NULL)
+	{
+		ft_printf("%i ", tmp->nb);
+		tmp = tmp->next;
+	}
+	ft_printf("\n");
+	tmp = data.stack_a->end;
+	while (tmp != NULL)
+	{
+		ft_printf("%i ", tmp->nb);
+		tmp = tmp->prev;
+	}
+	ft_printf("\n");
+	free(data.stack_a);
 	return (0);
 }
---
+++ studies/push_swap_args_check.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_args_check.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 21:46:07 by sguilher          #+#    #+#             */
+/*   Updated: 2022/04/12 22:25:42 by sguilher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ps_error(long int *nbs)
+{
+	if (nbs != NULL)
+		free(nbs);
+	ft_putstr_fd("Error\n", 2);
+	exit(EXIT_FAILURE);
+}
+
+void	ps_check_order(int stack_size, long int *nbs)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack_size - 1)
+	{
+		if (nbs[i] > nbs[i + 1])
+			return ;
+		i++;
+	}
+	free(nbs);
+	exit(EXIT_SUCCESS);
+}
+
+void	ps_check_repeated_nb(int stack_size, long int *nbs)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < stack_size)
+	{
+		j = i + 1;
+		while (j < stack_size)
+		{
+			if (nbs[i] == nbs[j])
+				ps_error(nbs);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	ps_check_char(char *nb)
+{
+	int	j;
+	int	nb_size;
+
+	j = 0;
+	nb_size = ft_strlen(nb);
+	while (j < nb_size)
+	{
+		if (!ft_isdigit(nb[j]))
+		{
+			if (nb[j] != '-' && nb[j] != '+')
+				return (E_NOT_INT);
+			if ((nb[j] == '-' || nb[j] == '+') && nb_size == 1)
+				return (E_NOT_INT);
+			if ((nb[j] == '-' || nb[j] == '+') && j > 1)
+				return (E_NOT_INT);
+		}
+		j++;
+	}
+	return (OK);
+}
+
+int	*ps_check_args(int stack_size, char *numbers[])
+{
+	int	i;
+	long int	*nbs;
+
+	nbs = (long int *) malloc(stack_size * sizeof(long int));
+	if (nbs == NULL)
+		ps_error(NULL);
+	i = 0;
+	while (i < stack_size)
+	{
+		if (ps_check_char(numbers[i]) == E_NOT_INT)
+			ps_error(nbs);
+		nbs[i] = ft_atol(numbers[i]);
+		if (nbs[i] > 2147483647 || nbs[i] < -2147483648)
+			ps_error(nbs);
+		i++;
+	}
+	if (stack_size == 1)
+	{
+		free(nbs);
+		exit(EXIT_SUCCESS);
+	}
+	ps_check_repeated_nb(stack_size, nbs);
+	ps_check_order(stack_size, nbs);
+	return ((int *)nbs);
+}
+
+int	main(int argc, char *argv[])
+{	
+	if (argc == 1)
+		return (0);
+	ps_check_args(argc - 1, &argv[1]);
+	return (0);
+}
