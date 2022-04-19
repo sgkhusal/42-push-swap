99d722e8636e7bb96996be6b49ce9ccdb9f94f8d
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-04-19 06:35:46 +0200

    Update media print from integer to float

--- tests/tester_n_cases.sh
+++ tests/tester_n_cases.sh
@@ -6,7 +6,7 @@
 #    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/18 20:17:58 by sguilher          #+#    #+#              #
-#    Updated: 2022/04/18 22:35:14 by sguilher         ###   ########.fr        #
+#    Updated: 2022/04/19 06:32:59 by sguilher         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,8 +17,8 @@ GREY="\033[38;5;244m"
 PURPLE="\033[1;35m"
 BLUE="\033[0;36m"
 END="\033[0m"
-QTY_TESTS=100 # change the total of tests
-n=5 # change the stack size
+QTY_TESTS=10 # change the total of tests
+n=4 # change the stack size
 
 cd .. # change path to push_swap
 
@@ -32,7 +32,6 @@ echo "******************* stack size = $n *******************"
 i=1
 SUM=0
 RET=0
-MED=0
 while [ $i -le $QTY_TESTS ]
 do
 	seq 1 $n | shuf | tr '\n' ' ' > numbers
@@ -52,9 +51,7 @@ do
 	SUM=$(($SUM + $RET))
 done
 
-printf "\n"
-MED=$(($SUM / $QTY_TESTS))
-printf "Media = $MED\n"
+printf "\nMedia = %.1f" "$(($SUM / $QTY_TESTS))"
 printf "\n$END"
 
 rm numbers result
