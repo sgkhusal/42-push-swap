f906069c78d126015e3d7309e0db652a02e01e9f
Sheela G. Khusal <sheela.khusal.08@gmail.com>
2022-04-21 21:31:31 +0200

    Add tests for some cases with high number of operations for size 6 and 7

--- tests/tester_6_cases.sh
+++ tests/tester_6_cases.sh
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    tester_n_cases.sh                                  :+:      :+:    :+:    #
+#    tester_6_cases.sh                                  :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/18 20:17:58 by sguilher          #+#    #+#              #
-#    Updated: 2022/04/21 21:30:03 by sguilher         ###   ########.fr        #
+#    Updated: 2022/04/21 21:21:38 by sguilher         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,39 +17,35 @@ GREY="\033[38;5;244m"
 PURPLE="\033[1;35m"
 BLUE="\033[0;36m"
 END="\033[0m"
-QTY_TESTS=100 # change the total of tests
-n=5 # change the stack size
 
 cd .. # change path to push_swap
 
-rm log$n
+rm log6
 
 printf "\n"
 printf "$YELLOW"
 echo "****************** PUSH SWAP TESTER ******************"
-echo "******************* stack size = $n *******************"
-printf "\n"
+echo "******************* stack size = 6 *******************"
+
 i=1
 SUM=0
 RET=0
-while [ $i -le $QTY_TESTS ]
+
+cat tests/6_cases | while read case6
 do
-	seq 1 $n | shuf | tr '\n' ' ' > numbers
-	printf "$PURPLE	Test $i: $GREY"
-	cat numbers
-	printf "\nTest $i: " >> log$n
-	cat numbers >> log$n
-	cat numbers | xargs ./push_swap > result
+	printf "$PURPLE\nTest $i:$GREY $case6"
+	printf "\nTest $i: $case6" >> log6
+	./push_swap $case6 > result
 	#cat result
 	RET=`cat result | wc -l | bc`
 	printf "$BLUE\nTotal moves = $RET\n" 
-	printf "\nTotal moves = $RET\n" >> log$n
+	printf "\nTotal moves = $RET\n" >> log6
 	i=$(($i + 1))
 	SUM=$(($SUM + $RET))
 done
 
-printf "\nMedia = %.1f" "$(($SUM / $QTY_TESTS))"
-printf "\nMedia = %.1f" "$(($SUM / $QTY_TESTS))" >> log$n
+printf "\nMedia = %.1f" "$(($SUM / $i))"
+printf "\nMedia = %.1f" "$(($SUM / $i))" >> log6
 printf "\n$END"
 
-rm numbers result
+rm result
--- tests/tester_7_cases.sh
+++ tests/tester_7_cases.sh
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    tester_n_cases.sh                                  :+:      :+:    :+:    #
+#    tester_7_cases.sh                                  :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/18 20:17:58 by sguilher          #+#    #+#              #
-#    Updated: 2022/04/21 21:30:03 by sguilher         ###   ########.fr        #
+#    Updated: 2022/04/21 04:46:55 by sguilher         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,39 +17,35 @@ GREY="\033[38;5;244m"
 PURPLE="\033[1;35m"
 BLUE="\033[0;36m"
 END="\033[0m"
-QTY_TESTS=100 # change the total of tests
-n=5 # change the stack size
 
 cd .. # change path to push_swap
 
-rm log$n
+rm log7
 
 printf "\n"
 printf "$YELLOW"
 echo "****************** PUSH SWAP TESTER ******************"
-echo "******************* stack size = $n *******************"
-printf "\n"
+echo "******************* stack size = 7 *******************"
+
 i=1
 SUM=0
 RET=0
-while [ $i -le $QTY_TESTS ]
+
+cat tests/7_cases | while read case7
 do
-	seq 1 $n | shuf | tr '\n' ' ' > numbers
-	printf "$PURPLE	Test $i: $GREY"
-	cat numbers
-	printf "\nTest $i: " >> log$n
-	cat numbers >> log$n
-	cat numbers | xargs ./push_swap > result
+	printf "$PURPLE\nTest $i:$GREY $case7"
+	printf "\nTest $i: $case7" >> log7
+	./push_swap $case7 > result
 	#cat result
 	RET=`cat result | wc -l | bc`
 	printf "$BLUE\nTotal moves = $RET\n" 
-	printf "\nTotal moves = $RET\n" >> log$n
+	printf "\nTotal moves = $RET\n" >> log7
 	i=$(($i + 1))
 	SUM=$(($SUM + $RET))
 done
 
-printf "\nMedia = %.1f" "$(($SUM / $QTY_TESTS))"
-printf "\nMedia = %.1f" "$(($SUM / $QTY_TESTS))" >> log$n
+printf "\nMedia = %.1f" "$(($SUM / $i))"
+printf "\nMedia = %.1f" "$(($SUM / $i))" >> log7
 printf "\n$END"
 
-rm numbers result
+rm result
