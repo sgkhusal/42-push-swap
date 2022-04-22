1e01bec6f0df125dc62336365c4163ad81d7b703
sgkhusal <sheela.khusal.08@gmail.com>
2022-04-21 23:52:47 -0300

    Add some tests cases for stack size = 5

---
+++ tests/tester_5_cases.sh
@@ -0,0 +1,51 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester_5_cases.sh                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/04/18 20:17:58 by sguilher          #+#    #+#              #
+#    Updated: 2022/04/21 21:21:38 by sguilher         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+YELLOW="\033[0;33m"
+GREY="\033[38;5;244m"
+PURPLE="\033[1;35m"
+BLUE="\033[0;36m"
+END="\033[0m"
+
+cd .. # change path to push_swap
+
+rm log5
+
+printf "\n"
+printf "$YELLOW"
+echo "****************** PUSH SWAP TESTER ******************"
+echo "******************* stack size = 5 *******************"
+
+i=1
+SUM=0
+RET=0
+
+cat tests/5_cases | while read case5
+do
+	printf "$PURPLE\nTest $i:$GREY $case5"
+	printf "\nTest $i: $case5" >> log5
+	./push_swap $case5 > result
+	#cat result
+	RET=`cat result | wc -l | bc`
+	printf "$BLUE\nTotal moves = $RET\n"
+	printf "\nTotal moves = $RET\n" >> log5
+	i=$(($i + 1))
+	SUM=$(($SUM + $RET))
+done
+
+printf "\nMedia = %.1f" "$(($SUM / $i))"
+printf "\nMedia = %.1f" "$(($SUM / $i))" >> log5
+printf "\n$END"
+
+rm result
